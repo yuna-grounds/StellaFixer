c47848012cf4a4d2023961175fed7f3d3eaa3f74
yuna-grounds <111099166+yuna-grounds@users.noreply.github.com>
2023-04-28 10:22:47 +0900

    CharMove_YN

---
+++ StellaFixer/Assets/B_Scripts_YN/CharMove_YN.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharMove_YN : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        // Thumbstick Left : 플레이어 이동
+        print("이동");
+        Vector2 dir_L = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
+        this.transform.Translate(Vector3.forward * Time.deltaTime * 5f * dir_L.y);
+
+        // Tumbstick_Right : 플레이어 회전
+        print("회전");
+        Vector2 dir_R = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.RTouch);
+        this.transform.Rotate(Vector3.up * Time.deltaTime * 90f * dir_R.x);
+
+    }
+}
